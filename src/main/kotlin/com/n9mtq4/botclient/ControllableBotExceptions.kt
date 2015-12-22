c88ee8d4c04e2ac034c5d1f06710bf1665a6aeee
n9Mtq4 <bill.wresnahan@gmail.com>
2015-12-21 20:41:05 -0500

    little change for backwards compatibility

--- src/main/kotlin/com/n9mtq4/botclient/ControllableBotExceptions.kt
+++ src/main/kotlin/com/n9mtq4/botclient/ControllableBotExceptions.kt
@@ -8,7 +8,7 @@ package com.n9mtq4.botclient
  * 
  * @author Will "n9Mtq4" Bresnahan
  */
-class NotEnoughActionPointsException(val msg: String) : Exception(msg) {
+class NotEnoughActionPointsException(msg: String) : CantPerformActionException(msg) {
 	
 	constructor(need: Int, have: Int, action: String) : this("Not enough action points to perform action: $action. Need: $need, have: $have, diff:${have - need}!")
 	
@@ -22,8 +22,19 @@ class NotEnoughActionPointsException(val msg: String) : Exception(msg) {
  *
  * @author Will "n9Mtq4" Bresnahan
  */
-class NotEnoughManaPointsException(val msg: String) : Exception(msg) {
+class NotEnoughManaPointsException(msg: String) : CantPerformActionException(msg) {
 	
 	constructor(need: Int, have: Int, action: String) : this("Not enough mana points to perform action: $action. Need: $need, have: $have, diff:${have - need}!")
 	
 }
+
+/**
+ * Created by will on 12/21/15 at 8:40 PM.
+ *
+ * This exception is thrown when you can't perform an action.
+ * The superclass for [NotEnoughActionPointsException] and
+ * [NotEnoughManaPointsException].
+ *
+ * @author Will "n9Mtq4" Bresnahan
+ */
+open class CantPerformActionException(val msg: String) : Exception(msg)
